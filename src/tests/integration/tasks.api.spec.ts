7c456fcd10f9368de36dd77441052730f7072e00
Jin Zhang <jinzh235@gmail.com>
2023-05-21 21:37:41 +1200

    test1

---
+++ src/tests/integration/tasks.api.spec.ts
@@ -0,0 +1,26 @@
+// import { Server } from 'http'
+import app from '../../app'
+import request from 'supertest'
+
+//tasks.api.spec.ts
+//test-integration
+describe('Tasks API', () => {
+  test('it should return all tasks', async () => {
+    // Arrange
+    const expected = [
+      {
+        id: 1,
+        name: 'Mission 01',
+        description: 'Chatbot',
+        isCompleted: false,
+      },
+    ]
+
+    // Act
+    const res = await request(app).get('/api/tasks')
+
+    // Assert
+    expect(res.status).toEqual(200)
+    expect(res.body).toEqual(expected)
+  })
+})
